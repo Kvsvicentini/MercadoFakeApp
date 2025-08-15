ce85a6366ea6baa0e0b9eed919a97bce0459bfc7
Kvsvicentini <kaua.vicentini@gmail.com>
2025-08-15 20:07:30 -0300

    Continuando aula passada

--- app/src/main/java/br/com/etecia/myapp/ListaProdutosActivity.java
+++ app/src/main/java/br/com/etecia/myapp/ListaProdutosActivity.java
@@ -7,8 +7,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListaProdutosActivity extends AppCompatActivity {
+
+    RecyclerView idListaProdutos;
+    List<Produtos> lstProdutos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,21 @@ public class ListaProdutosActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //criando um array que vai carregar a lista de produtos
+        lstProdutos = new ArrayList<>();
+        lstProdutos.add(new Produtos("Mouse",R.drawable.mousejogo));
+        lstProdutos.add(new Produtos("Teclado",R.drawable.teclado));
+        lstProdutos.add(new Produtos("VolanteAcessorio",R.drawable.volanteacessorio));
+        lstProdutos.add(new Produtos("Videogame",R.drawable.videogame));
+        lstProdutos.add(new Produtos("Suporte",R.drawable.suportepara));
+        lstProdutos.add(new Produtos("Fone",R.drawable.fone));
+        lstProdutos.add(new Produtos("Jogo",R.drawable.jogo));
+        lstProdutos.add(new Produtos("Carregador",R.drawable.carregador));
+        lstProdutos.add(new Produtos("Console",R.drawable.console));
+        lstProdutos.add(new Produtos("Controle",R.drawable.controle_videogame));
+
+        idListaProdutos = findViewById(R.id.ListaProdutos);
+
     }
 }
